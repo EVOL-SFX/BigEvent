953a3f1d8e5e83dc37f991e34ea34e41b869040b
EVOL-SFX <56402321+EVOL-SFX@users.noreply.github.com>
2019-10-13 20:28:49 +0800

    首页退出功能

--- web_back/tool/user.js
+++ web_back/tool/user.js
@@ -9,4 +9,12 @@ var user = {
             success: options.callback
         });
     },
+
+    logout:function(options){
+        $.ajax({
+            type:'post',
+            url: USER_LOGOUT,
+            success:options.callback
+        })
+    },
 }
